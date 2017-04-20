c0efbe3cda7b1910d800032e57c06b3836e0dff1
Maciej Gibas <gibasmaciej@gmail.com>
2017-04-20 17:29:54 -0400

    Cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,8 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
 'use strict';
 const del = require('del');
 const gulp = require('gulp');
-const gulpif = require('gulp-if');
-const uglify = require('gulp-uglify');
-const babel = require('gulp-babel');
-const imagemin = require('gulp-imagemin');
-const htmlmin = require('gulp-html-minifier');
+const $ = require('gulp-load-plugins')();
 const cssSlam = require('css-slam').gulp;
-const autoprefixer = require('gulp-html-autoprefixer');
 const mergeStream = require('merge-stream');
 const polymerBuild = require('polymer-build');
 const swPrecacheConfig = require('./sw-precache-config.js');
@@ -41,23 +27,25 @@ function build() {
     del([buildDirectory])
       .then(() => {
         let sourcesStream = project.sources()
+          .pipe($.plumber())
           .pipe(sourcesHtmlSplitter.split())
-          .pipe(gulpif(/\.js$/, babel({
-            presets: ['es2015']
-          })))
-          .pipe(gulpif(/\.js$/, uglify()))
-          .pipe(gulpif(/\.html$/, autoprefixer()))
-          .pipe(gulpif(/\.html$/, cssSlam()))
-          .pipe(gulpif(/\.html$/, htmlmin({ removeComments: true })))
-          .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
+          .pipe($.if(/\.js$/, $.babel({presets: ['es2015']})))
+          .pipe($.if(/\.js$/, $.uglify()))
+          .pipe($.if(/\.html$/, $.htmlAutoprefixer()))
+          .pipe($.if(/\.html$/, cssSlam()))
+          .pipe($.if(/\.html$/, $.htmlMinifier({ removeComments: true })))
+          .pipe($.if(/\.(png|gif|jpg|svg)$/, $.imagemin()))
           .pipe(sourcesHtmlSplitter.rejoin());
+
         let dependenciesStream = project.dependencies()
+          .pipe($.plumber())
           .pipe(dependenciesHtmlSplitter.split())
-          .pipe(gulpif(/\.js$/, uglify()))
-          .pipe(gulpif(/\.html$/, autoprefixer()))
-          .pipe(gulpif(/\.html$/, cssSlam()))
-          .pipe(gulpif(/\.html$/, htmlmin({ removeComments: true })))
+          .pipe($.if(/\.js$/, $.uglify()))
+          .pipe($.if(/\.html$/, $.htmlAutoprefixer()))
+          .pipe($.if(/\.html$/, cssSlam()))
+          .pipe($.if(/\.html$/, $.htmlMinifier({ removeComments: true })))
           .pipe(dependenciesHtmlSplitter.rejoin());
+
         let bundled = mergeStream(sourcesStream, dependenciesStream)
           .pipe(project.bundler)
           .pipe(gulp.dest('build/'));
